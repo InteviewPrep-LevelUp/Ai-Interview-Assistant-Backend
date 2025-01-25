3b882bc827c60ca3fd2d21a87581bf0fa50efbb8
Daniar Makyev <daniarmakyev10@gmail.com>
2025-01-25 20:21:20 +0600

    cors turned off

--- app.js
+++ app.js
@@ -19,7 +19,12 @@ const limiter = rateLimit({
 });
 
 app.use(limiter);
-app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 async function getOrCreateAssistant() {
   try {
